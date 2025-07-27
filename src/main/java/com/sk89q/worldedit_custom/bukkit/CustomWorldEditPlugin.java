3766d533d72bf8fb4eeb5e69d4500904cbf71047
Namiken <namiken2525@yahoo.co.jp>
2025-07-27 14:01:27 +0900

    OPがないとWEのコマンドを実行でき内容に修正

--- src/main/java/com/sk89q/worldedit_custom/bukkit/CustomWorldEditPlugin.java
+++ src/main/java/com/sk89q/worldedit_custom/bukkit/CustomWorldEditPlugin.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 
@@ -15,8 +16,25 @@ public class CustomWorldEditPlugin extends WorldEditPlugin {
   public List<String> onTabComplete(CommandSender sender, Command cmd, String commandLabel, String[] args) {
     //World EditのTabCompleteで鯖落ちすることがあるので無効化する
     System.out
-        .println("WorldEdit Tag Canceled!!: sender:" + sender.getName() + ", cmd:" + cmd.getName() + ", commandLabel" +
+        .println("[warn]WorldEdit Tag Canceled!!: sender:" + sender.getName() + ", cmd:" + cmd.getName()
+            + ", commandLabel:" +
             commandLabel + ", args:" + Arrays.toString(args));
     return new ArrayList<String>();
+  }
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+    if (!(sender instanceof Player)) { return super.onCommand(sender, cmd, commandLabel, args); }
+
+    boolean op = sender.isOp();
+    if (op) { return super.onCommand(sender, cmd, commandLabel, args); }
+
+    // OP持っていない場合はコマンドを実行しない
+    System.out
+        .println(
+            "[warn]WorldEdit Command Canceled!!: sender:" + sender.getName() + ", cmd:" + cmd.getName()
+                + ", commandLabel:" +
+                commandLabel + ", args:" + Arrays.toString(args));
+    return false;
   }
 }
